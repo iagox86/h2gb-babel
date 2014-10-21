9c40993a0242b1da6b9c7ab3efe2265974d35178
Ron <ron@skullsecurity.net>
2014-10-21 10:28:05 -0700

    Added some client functionality

--- Gemfile
+++ Gemfile
@@ -5,9 +5,3 @@ gem 'httparty'
 gem 'httmultiparty'
 
 gem 'metasm'      # For most of the disassembling / etc
-gem 'sinatra'     # Web server
-
-gem 'activerecord' # Database stuff
-gem 'sinatra-activerecord'
-gem 'sqlite3'
-
--- babel.rb
+++ babel.rb
@@ -1,202 +1,81 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
-require 'sinatra'
-require 'sinatra/activerecord'
-
-require 'binary'
-require 'memory'
-
-require 'fileutils'
 require 'json'
-require 'tempfile'
+require 'httparty'
 
 require 'pp' # debug
 
-FORCE_TEXT = true
+HOST = ARGV[0] || "http://localhost:9292"
 
-# Database stuff
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :host    => nil,
-  :username => nil,
-  :password => nil,
-  :database => 'data.db',
-  :encoding => 'utf8',
-)
+def do_get(url)
+  result = HTTParty.get(HOST + url)
 
-class Babel < Sinatra::Application
-  def add_status(status, table)
-    table[:status] = status
-    return table
-  end
+  return JSON.parse(result.body, :symbolize_names => true)
+end
 
-  def convert_num(n)
-    if(n > 0xFFFF)
-      return '0x%08x' % n
-    elsif(n > 0xFF)
-      return '0x%04x' % n
-    elsif(n > 0)
-      return '0x%02x' % n
-    elsif(n == 0)
-      return '0'
-    else
-      return "-%s" % (convert_num(n.abs()))
-    end
-  end
+def do_post(url, body)
+  result = HTTParty.post(HOST + url,
+    :body => JSON.generate(body),
+    :headers => {
+      'Content-Type' => 'application/json',
+    }
+  )
 
-  def convert_to_text!(h)
-    if(h.is_a?(Hash))
-      h.each do |k, v|
-        if(v.is_a?(Fixnum))
-          h[k] = convert_num(v)
-        elsif(v.is_a?(Hash) || v.is_a?(Array))
-          convert_to_text!(v)
-        end
-      end
-    elsif(h.is_a?(Array))
-      h.each_index do |k|
-        v = h[k]
-        if(v.is_a?(Fixnum))
-          h[k] = convert_num(v)
-        elsif(v.is_a?(Hash) || v.is_a?(Array))
-          convert_to_text!(v)
-        end
-      end
-    end
-  end
+  return JSON.parse(result.body, :symbolize_names => true)
+end
 
-  def Babel.COMMAND(c)
-    return /^\/#{c}\/([a-fA-F0-9-]+)$/
-  end
+def do_delete(url)
+  result = HTTParty.delete(HOST + url)
 
-  # Add important headers and encode everything as JSON
-  after do
-    if(response.content_type.nil?)
-      content_type 'application/json'
-    end
+  return JSON.parse(result.body, :symbolize_names => true)
+end
 
-    headers({ 'X-Frame-Options' => 'DENY' })
+def binary_upload(filename)
+  name = File.basename(filename)
+  comment = "Uploaded by h2gb-babel"
 
-    if(response.content_type =~ /json/)
-      headers({
-#        'Access-Control-Allow-Origin' => '*', # TODO: Might not need this forever
-        'Content-Disposition' => 'Attachment'
-      })
+  return do_post("/binary/upload", {
+                  :name => name,
+                  :comment => comment,
+                  :data => IO.read(filename).force_encoding('ISO-8859-1'),
+                })
+end
 
-      if(FORCE_TEXT)
-        convert_to_text!(response.body)
-      end
+def binary_list()
+  return do_get("/binaries")[:binaries]
+end
 
-      response.body = JSON.pretty_generate(response.body) + "\n"
-    end
-  end
+def binary_delete(id)
+  return do_delete("/binary/%d" % id)
+end
 
-  # Handle errors (exceptions and stuff)
-  error do
-    status 200
+def binary_delete_all()
+  binaries = binary_list()
 
-    return add_status(500,  {:reason => env['sinatra.error'] })
-  end
+  puts(binaries.inspect)
 
-  # Handle file-not-found errors
-  not_found do
-    status 404
-
-    return add_status(404, {:reason => 'not found' })
-  end
-
-  get '/' do
-    return "Welcome to h2gb!"
-  end
-
-  post '/upload_html' do
-    content_type 'text/html'
-
-    b = Binary.new(
-      :name => params['file'][:filename],
-      :comment => params['comment'],
-      :data => params['file'][:tempfile].read()
-    )
-    b.save()
-
-
-    redirect to('/static/test.html#' + b.id)
-  end
-
-  post '/upload' do
-    b = Binary.new(
-      :name         => params['filename'],
-      :comment      => params['comment'],
-      :data         => params['data'],
-      :is_processed => false,
-    )
-    b.save()
-
-    return add_status(0, {:id => b.id })
-  end
-
-  get('/binaries') do
-    return add_status(0, {:binaries => Binary.all().as_json() })
-  end
-
-  get(COMMAND('process')) do |id|
-    b = Binary.find(id)
-    b.process()
-    b.save()
-  end
-
-  get(COMMAND('download')) do |id|
-    b = Binary.find(id)
-    return add_status(0, {
-      :name => b.filename,
-      :file => Base64.encode64(b.data),
-    })
-  end
-
-  get(COMMAND('parse')) do |id|
-    b = Binary.find(id)
-
-    return add_status(0, b.details())
-  end
-
-  get(COMMAND('delete')) do |id|
-    b = Binary.find(id)
-    b.destroy()
-  end
-
-  get(COMMAND('format')) do |id|
-    b = Binary.find(id)
-    return add_status(0, { format: b.format() })
-  end
-
-  get(COMMAND('disassemble')) do |id|
-    b = Binary.find(id)
-
-    offset = params['offset']
-    length = params['length']
-    arch = params['arch']
-
-    if(!offset.nil?)
-      offset = offset.to_i()
-    end
-    if(!length.nil?)
-      length = length.to_i()
-    end
-
-    return add_status(0, {
-      :instructions => b.disassemble(offset, length, arch)
-    })
-  end
-
-  get(/\/static\/([a-zA-Z0-9_-]+\.[a-zA-Z0-9_-]+)/) do |file|
-    if(file =~ /\.html$/)
-      content_type "text/html"
-    elsif(file =~ /\.js$/)
-      content_type "text/javascript"
-    else
-      raise(Exception, "Unknown filetype")
-    end
-
-    return IO.read(File.dirname(__FILE__) + "/static/#{file}")
+  binaries.each do |b|
+    binary_delete(b[:id])
   end
 end
+
+def binary_download(binary_id)
+  return do_get("/binary/%d/download" % binary_id)
+end
+
+puts("Uploading a test binary")
+binary = binary_upload("./sample.raw")
+binary_id = binary[:binary_id]
+puts("ID: %d" % binary_id)
+
+puts("Listing binaries (we should see the one):")
+puts(binary_list())
+
+puts("Downloading it:")
+puts(binary_download(binary_id))
+
+puts("Deleting it")
+binary_delete(binary_id)
+
+puts("Listing binaries (this should be empty):")
+puts(binary_list())
