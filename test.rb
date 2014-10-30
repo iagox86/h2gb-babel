a9946160771fbb97672d3aaa37143eb4441b8b98
Ron <ron@skullsecurity.net>
2014-10-30 13:44:26 -0700

    Continued development on a better interface to the RESTful API

--- test.rb
+++ test.rb
@@ -4,31 +4,6 @@ require 'cgi' # for URI::encode()
 
 HOST = ARGV[0] || "http://localhost:9292"
 
-class Binary < ActiveRestClient::Base
-  base_url HOST
-  request_body_type :json
-
-  get  :all,              "/binaries"
-  get  :find,             "/binary/:id"
-  put  :save,             "/binary/:id"
-  post :create,           "/binary"
-
-  before_request do |name, request|
-    # Convert 'data' to base64, if it's present
-    if(!request.post_params[:data].nil?)
-      request.post_params[:data] = Base64.encode64(request.post_params[:data])
-    end
-  end
-
-  def Binary.find(id)
-    b = super(id)
-
-    b.data = Base64.decode64(b.data)
-
-    return b
-  end
-end
-
 module ActiveRestExtras
   def format_url(url, params)
     @attributes.each_pair do |k, v|
@@ -61,23 +36,51 @@ module ActiveRestExtras
   end
 end
 
+class Binary < ActiveRestClient::Base
+  base_url HOST
+  request_body_type :json
+
+  get    :all,    "/binaries"
+  get    :find,   "/binaries/:binary_id"
+  put    :save,   "/binaries/:binary_id"
+  post   :create, "/binaries"
+  delete :delete, "/binaries/:binary_id"
+
+  # Transparently encode base64 in outbound requests
+  before_request do |name, request|
+    # Convert 'data' to base64, if it's present
+    if(!request.post_params[:data].nil?)
+      request.post_params[:data] = Base64.encode64(request.post_params[:data])
+    end
+  end
+
+  # Transparently decode base64 in the server's response
+  def Binary.find(id)
+    b = super(id)
+    b.data = Base64.decode64(b.data)
+    return b
+  end
+end
+
+
 class Workspace < ActiveRestClient::Base
   base_url HOST
   request_body_type :json
 
   include ActiveRestExtras
 
-  get  :all,    "/binary/:binary_id/workspaces"
-  post :create, "/binary/:binary_id/create_workspace"
-  get  :find,   "/workspace/:id"
-  put  :save,   "/workspace/:id"
+  get    :all,            "/binaries/:binary_id/workspaces"
+  get    :find,           "/workspaces/:workspace_id"
+  put    :save,           "/workspaces/:workspace_id"
+  post   :create,         "/binaries/:binary_id/new_workspace"
+  delete :delete,         "/workspaces/:workspace_id"
 
   def set(params)
-    return post_stuff("/workspace/:id/set", params)
+    return post_stuff("/workspace/:workspace_id/set", params)
   end
 
   def get(params)
-    result = get_stuff("/workspace/:id/get", params)
+    result = get_stuff("/workspace/:workspace_id/get", params)
 
     return result.value
   end
@@ -87,15 +90,155 @@ class Memory < ActiveRestClient::Base
   base_url HOST
   request_body_type :json
 
-  get  :all,    "/workspace/:workspace_id/memories"
-  post :create, "/workspace/:workspace_id/create_memory"
-  get  :find,   "/memory/:id"
-  put  :save,   "/memory/:id"
+  get    :all,            "/workspaces/:workspace_id/memories"
+  get    :find,           "/memory/:memory_id"
+  put    :save,           "/memory/:memory_id"
+  post   :create,         "/workspaces/:workspace_id/new_memory"
+  delete :delete,         "/binaries/:memory_id"
+
+  def new_segment(name, address, file_address, data)
+    return post_stuff("/memory/:memory_id/new_segment", {
+      :name         => name,
+      :address      => address,
+      :file_address => file_address,
+      :data         => Base64.encode64(data),
+    })
+  end
+
+  def delete_segment(name)
+    return post_stuff("/memory/:memory_id/delete_segment", {
+      :segment => name,
+    })
+  end
+
+  def new_node(address, type, length, value, details, references)
+    return post_stuff("/memory/:memory_id/create_node", {
+      :address      => address,
+      :type         => type,
+      :length       => length,
+      :value        => value,
+      :details      => details,
+      :references   => references,
+    })
+  end
+
+  def delete_node()
+    return post_stuff("/memory/:memory_id/delete_segment", {
+      :segment => name,
+    })
+  end
 end
 
-x = Workspace.find(3)
-puts(x.inspect)
+puts()
+puts("** CREATE A BINARY")
+binary = Binary.create(
+  :name => "Binary Test",
+  :comment => "Test binary",
+  :data => Base64.encode64("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+)
+if(!binary.id)
+  puts("A valid binary wasn't returned!")
+  puts("We got: #{binary.inspect}")
+  exit
+end
+puts(binary.inspect)
+puts("BINARY_ID: %d" % binary.id)
 
-puts x.set(:name => "abc", :value => "hihihi")
+puts()
+puts("** GET ALL BINARIES (should include the new one)")
+all_binaries = Binary.all()
+good = false
+all_binaries.binaries.each do |b|
+  if(b.id == binary.id)
+    good = true
+    puts("  * Found our new binary!")
+  end
+end
+if(!good)
+  puts("Couldn't find out new binary!")
+  exit
+end
 
-puts(x.get(:name => "abc"))
+puts()
+puts("** FIND A BINARY")
+binary_again = Binary.find(:binary_id => binary.id)
+puts(binary_again.inspect)
+binary = binary_again
+
+# TODO: Test the update
+
+puts()
+puts("** CREATE A WORKSPACE")
+workspace = Workspace.create(:binary_id => binary.id, :name => "test workspace")
+puts(workspace.inspect)
+if(!workspace.id)
+  puts("A valid workspace wasn't returned!")
+  puts("We got: #{workspace.inspect}")
+  exit
+end
+puts("WORKSPACE_ID: %d" % workspace.id)
+
+puts()
+puts("** LIST ALL WORKSPACES")
+workspaces = Workspace.all(:binary_id => binary.id)
+if(workspaces[:workspaces].count != 1)
+  puts("We should have exactly one workspace under our new binary, but we have #{workspaces[:workspaces].length} instead!")
+  exit
+end
+workspaces[:workspaces].each do |w|
+  if(w.id != workspace.id)
+    puts("The workspace returned by 'all' didn't have the same id as the new workspace!")
+    exit
+  end
+  puts(w.inspect)
+end
+
+puts()
+puts("** FIND THE WORKSPACE")
+workspace = Workspace.find(:workspace_id => workspace.id)
+puts(workspace.inspect)
+
+# TODO: Test the update
+
+# TODO: Test the get / set
+
+puts()
+puts("** CREATE MEMORY")
+memory = Memory.create(:workspace_id => workspace.id)
+puts(memory.inspect)
+
+puts()
+puts("** LIST MEMORIES")
+memories = Memory.all(:workspace_id => workspace.id)
+puts(memories.inspect)
+if(memories[:memories].count != 1)
+  puts("Exactly 1 result wasn't returned as expected! Instead, #{memories[:memories].count} were returned")
+end
+memories[:memories].each do |m|
+  if(m.id != memory.memory_id)
+    puts("The memory returned by 'all' didn't have the same id as the new memory!")
+    puts(m.id)
+    puts(memory.memory_id)
+    exit
+  end
+end
+
+
+puts()
+puts("** FIND MEMORY")
+memory = Memory.find(:memory_id => memory.memory_id)
+puts(memory.inspect)
+
+
+# TODO: Create segments / nodes
+
+#puts()
+#puts("** DELETING MEMORY")
+#
+#puts("** DELETE THE WORKSPACE")
+#puts(workspace.inspect)
+#puts workspace.delete()
+#
+#puts()
+#puts("** DELETE THE BINARY")
+#puts binary.delete().inspect
