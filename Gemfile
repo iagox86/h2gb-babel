89d7c521e1ed017621edd5ada45d3ea9a4251048
Ron <ron@skullsecurity.net>
2014-12-23 18:29:27 -0800

    Added an ugly workaround for a rack bug

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'httparty' # For all the networky stuff
 gem 'metasm'   # For most of the disassembling / etc
+gem 'trollop'  # Parsing commands
--- models/model.rb
+++ models/model.rb
@@ -35,7 +35,7 @@ class Model
     puts("URL: #{url}")
 
     if(use_body)
-      result = method.call(url, :body => JSON.pretty_generate(params))
+      result = method.call(url, :body => "BASE64" + Base64.encode64(JSON.pretty_generate(params)))
     else
       new_params = []
       params.each_pair do |k, v|
